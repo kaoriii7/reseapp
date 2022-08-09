f59c9375d98b1b3b7acb06a38ee099210ddc88b0
kaoriii7 <nakano.7034k@gmail.com>
2022-08-10 07:16:42 +0900

    before like function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
           ShopsTableSeeder::class,
-          AreasTableSeeder::class,
-          GenresTableSeeder::class,
+          // AreasTableSeeder::class,
+          // GenresTableSeeder::class,
         ]);
     }
 }
