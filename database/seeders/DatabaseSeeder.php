966c380b0b27755af18ecc80e4854b70d2663071
kaoriii7 <nakano.7034k@gmail.com>
2022-08-25 00:32:46 +0900

    heroku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,16 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-<<<<<<< HEAD
-          // ShopsTableSeeder::class,
-          // AreasTableSeeder::class,
-          // GenresTableSeeder::class,
-          LikesTableSeeder::class,
-=======
           ShopsTableSeeder::class,
-          // AreasTableSeeder::class,
-          // GenresTableSeeder::class,
->>>>>>> f59c9375d98b1b3b7acb06a38ee099210ddc88b0
+          AreasTableSeeder::class,
+          GenresTableSeeder::class,
+          LikesTableSeeder::class,
         ]);
     }
 }
