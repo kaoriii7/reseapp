9a21ded28d4d9deb720a609da83518b9f08b6d97
kaoriii7 <nakano.7034k@gmail.com>
2022-08-27 21:11:42 +0900

    fix

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -30,7 +30,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string', 'regex:/^[a-zA-Z0-9]+$/'],
+            'password' => ['required', 'string'],
         ];
     }
 
