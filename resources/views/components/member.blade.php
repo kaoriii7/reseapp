fd8627fbf63d82966f41086b83789c721d6887fb
kaoriii7 <nakano.7034k@gmail.com>
2022-08-27 22:41:29 +0900

    fix

--- resources/views/components/member.blade.php
+++ resources/views/components/member.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/style.css">
     <script src="https://kit.fontawesome.com/18426ca9a1.js" crossorigin="anonymous"></script>
   </head>
 
@@ -15,11 +15,11 @@
         <div class="menu-wrap">
           <nav class="nav" id="nav">
             <ul>
-              <li><a href="/">Home</a></li>
+              <li class="nav__list"><a href="/">Home</a></li>
               <form action="{{ route('logout') }}" method="get">
-              <button class="list" type="submit">Logout</button>
+              <button class="nav__list" type="submit">Logout</button>
               </form>
-              <li><a href="/mypage">Mypage</a></li>
+              <li class="nav__list"><a href="/mypage">Mypage</a></li>
             </ul>
           </nav>
           <div class="menu" id="menu">
